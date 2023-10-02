1d50266067162e7cc0ec6460924eb73f53afa7d3
Pedro El-Khouri <pedroelkhouri@gmail.com>
2023-10-02 00:11:56 -0300

    Database creation

--- src/main/java/project/demo/series/domain/Series.java
+++ src/main/java/project/demo/series/domain/Series.java
@@ -1,12 +1,21 @@
 package project.demo.series.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Series {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     private Long id;
     @JsonProperty("name")
     private String nameSeries;
