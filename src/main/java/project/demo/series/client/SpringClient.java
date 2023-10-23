281ccd24620761fd2782c173a73e791266727c53
Pedro El-Khouri <pedroelkhouri@gmail.com>
2023-10-23 15:51:09 -0300

    RestTemplate POST, PUT & DELETE

--- src/main/java/project/demo/series/client/SpringClient.java
+++ src/main/java/project/demo/series/client/SpringClient.java
@@ -2,8 +2,7 @@ package project.demo.series.client;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import project.demo.series.domain.Series;
 
@@ -30,5 +29,33 @@ public class SpringClient {
                 "http://localhost:8080/series/all", HttpMethod.GET, null, new ParameterizedTypeReference<>(){});
         //formatter:on
         log.info(exchange.getBody());
+
+//        Series houseOfDragon = Series.builder().name("House of Dragon").build();
+//        Series houseOfDragonSaved = new RestTemplate().postForObject(
+//              "http://localhost:8080/series/", houseOfDragon, Series.class);
+//        log.info("Saved Series '{}'", houseOfDragonSaved);
+
+        Series suits = Series.builder().name("Suits").build();
+        ResponseEntity<Series> suitsSaved = new RestTemplate().exchange(
+                "http://localhost:8080/series", HttpMethod.POST, new HttpEntity<>(suits, httpJsonHeaders()), Series.class);
+        log.info("Saved Series '{}'", suitsSaved);
+
+        Series seriesToBeUpdated = suitsSaved.getBody();
+        seriesToBeUpdated.setName("Suits 2");
+
+        ResponseEntity<Void> suitsUpdated = new RestTemplate().exchange(
+                "http://localhost:8080/series", HttpMethod.PUT, new HttpEntity<>(seriesToBeUpdated, httpJsonHeaders()), Void.class);
+        log.info(suitsUpdated);
+
+        ResponseEntity<Void> suitsDeleted = new RestTemplate().exchange(
+                "http://localhost:8080/series/{id}", HttpMethod.DELETE, null, Void.class, seriesToBeUpdated.getId());
+        log.info(suitsDeleted);
+
+    }
+
+    private static HttpHeaders httpJsonHeaders() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
--- src/main/java/project/demo/series/dto_requests/SeriesPostRequestBody.java
+++ src/main/java/project/demo/series/dto_requests/SeriesPostRequestBody.java
@@ -1,10 +1,12 @@
 package project.demo.series.dto_requests;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class SeriesPostRequestBody {
     @NotEmpty(message = "The name of this series cannot be empty")
+    @NotNull
     private String name;
 }
