4351fae29eec24e41994323e8b92cc26652900c0
Pedro El-Khouri <pedroelkhouri@gmail.com>
2023-11-19 00:01:35 -0300

    Grafana

--- src/main/java/project/demo/series/SeriesApplication.java
+++ src/main/java/project/demo/series/SeriesApplication.java
@@ -1,7 +1,11 @@
 package project.demo.series;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SeriesApplication {
@@ -10,4 +14,9 @@ public class SeriesApplication {
 		SpringApplication.run(SeriesApplication.class, args);
 	}
 
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
 }
